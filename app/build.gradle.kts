d0a7c6d6edacf9095675a508ed5d79012f653ad0
charlysaenzx2657-wq <charlysaenzx2657@gmail.com>
2026-05-12 20:12:03 -0600

    fix dataBinding y gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,8 @@ android {
     }
     kotlinOptions { jvmTarget = "1.8" }
 
-    buildFeatures { viewBinding = true }
+    buildFeatures { viewBinding = true
+    dataBinding = false }
 }
 
 dependencies {
